a5b62f822d9b65505694af34987059dae6e0a2ac
Zhiyuan Guo <zhiyuan.guo@c3.ai>
2023-02-09 15:51:00 -0800

    .

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='ColBERT',
+    name='ColBERT1',
     version='0.2.0',
     author='Omar Khattab',
     author_email='okhattab@stanford.edu',
     description="Efficient and Effective Passage Search via Contextualized Late Interaction over BERT",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/stanford-futuredata/ColBERT',
+    url='https://github.com/c3-zyguo/ColBERT',
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
 )
